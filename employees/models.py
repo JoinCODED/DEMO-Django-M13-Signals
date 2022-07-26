f7598c57d9bad68963006f597b1d446cb08f780d
Mohammad Al-Thunayan <malthunayan4@gmail.com>
2022-07-26 19:52:01 +0300

    Add boilerplate

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_employee = models.BooleanField()
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Employee(models.Model):
+    department = models.ForeignKey(
+        Department,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="employees",
+    )
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="employee",
+    )
+
+    def __str__(self) -> str:
+        return str(self.user)
--- hr/settings.py
+++ hr/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "employees",
 ]
+
+AUTH_USER_MODEL = "employees.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
